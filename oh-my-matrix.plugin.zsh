b42bd1c78d61c2af17e819313750d7530dfc3a65
lee <loyalpartner@163.com>
2019-04-29 13:43:53 +0800

    fix  antigen can't find matrix.py
    fix antigen can't find matrix.py

--- oh-my-matrix.plugin.zsh
+++ oh-my-matrix.plugin.zsh
@@ -1,2 +1,2 @@
-#Oh-my-ZSH Matrix plugin based on Corey Golderg Python script : http://coreygoldberg.blogspot.com/2013/01/python-matrix-in-your-terminal.html
+# Oh-my-ZSH Matrix plugin based on Corey Golderg Python script : http://coreygoldberg.blogspot.com/2013/01/python-matrix-in-your-terminal.html
 alias matrix="python $(cd `dirname $0`; pwd)/matrix.py"
